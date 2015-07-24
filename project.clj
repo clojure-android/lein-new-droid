750087a087167f49845eb46db609a5bdaf3752a4
Alexander Yakushev <alex@bytopia.org>
2015-07-24 16:12:14 +0300

    Rewrite to use embedded dependency resolution

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject droid/lein-template "0.1.0"
+(defproject droid/lein-template "0.1.2"
   :description "Shortcut template for creating lein-droid projects."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ancient-clj "0.3.9"]]
   :eval-in-leiningen true)
--- src/leiningen/new/droid.clj
+++ src/leiningen/new/droid.clj
@@ -1,24 +1,27 @@
 (ns leiningen.new.droid
   (:require [leiningen.core.main :as main]
-            [cemerick.pomegranate :as deps]
-            [ancient-clj.core :as ancient]))
+            [leiningen.core.classpath :as cp]
+            [leiningen.core.project :as project]))
 
-(defn latest-lein-droid-version []
-  (-> (ancient/versions! 'lein-droid {:snapshots? false})
-      first :version-string))
-
-(defn pull-lein-droid [version]
-  (deps/add-dependencies :coordinates [['lein-droid version]]
-                         :repositories {"clojars" "http://clojars.org/repo"}))
+(defn pull-lein-droid
+  "Downloads latest lein-droid plugin from Clojars, adds it to the classpath,
+  returns its version."
+  []
+  (->> (cp/get-dependencies :dependencies {:dependencies [['lein-droid "RELEASE"]]
+                                           :repositories project/default-repositories}
+                            :add-classpath? true)
+       keys
+       (some #(when (= (first %) 'lein-droid) %))
+       second))
 
 (defn droid
   "Creates a new Clojure-Android project using lein-droid. Usage: `lein new
   droid <project-name> <package> & options`. If called simply as `lein new
   droid :init`, it will initialize a library project in the current directory."
   [& args]
-  (let [version (latest-lein-droid-version)]
-    (pull-lein-droid version)
+  (let [version (pull-lein-droid)]
     (main/info (str "Using lein-droid " version))
+    (require 'leiningen.droid.new)
     (if (= (first args) :init)
       ((resolve 'leiningen.droid.new/init))
       (apply (resolve 'leiningen.droid.new/new) args))))
