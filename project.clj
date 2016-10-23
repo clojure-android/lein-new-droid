bd01e2b62eadebfd0550337bbfc120022ad08c33
Alexander Yakushev <alex@bytopia.org>
2016-10-23 11:46:42 +0300

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject droid/lein-template "0.1.8"
+(defproject droid/lein-template "0.2.0"
   :description "Shortcut template for creating lein-droid projects."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
